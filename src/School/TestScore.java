b28ad634d7a300b69a4ea50a8b8913fbfc44bf5d
Austin Sanderson <asanderson2@stu.jsu.edu>
2022-09-12 23:58:39 -0500

    Test All

---
+++ src/School/TestScore.java
@@ -0,0 +1,28 @@
+package School;
+
+
+public class TestScore {
+    int test1;
+    int test2;
+
+    public TestScore(int test1, int test2){
+        this.test1 = test1;
+        this.test2 = test2;
+    }
+
+    public int gettest1(){
+        return test1;
+    }
+
+    public void settest1(int test1){
+        this.test1 = test1;
+    }
+
+    public int gettest2(){
+        return test2;
+    }
+
+    public void settest2(int test2){
+        this.test2 = test2;
+    }
+}
